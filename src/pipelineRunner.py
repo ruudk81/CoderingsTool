daa03d62c48d78b00022b298349f82a35794ac98
Ruud Kooiman <r.kooiman@motivaction.nl>
2025-10-06 05:41:19 +0200

    app update - results in left column, if processed, and sample in right column

--- src/pipelineRunner.py
+++ src/pipelineRunner.py
@@ -1,7 +1,7 @@
 import os, sys; sys.path.extend([p for p in [os.getcwd().split('coderingsTool')[0] + suffix for suffix in ['', 'coderingsTool', 'coderingsTool/src', 'coderingsTool/src/utils']] if p not in sys.path]) if 'coderingsTool' in os.getcwd() else None
 
 import time
-import asyncio
+#import asyncio
 import pandas as pd
 import nest_asyncio
 from typing import List, Optional, Any, Tuple #Dict, Union
