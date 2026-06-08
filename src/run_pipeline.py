a93fd54fcd5bf445d6029c0f4debee5d899034e6
“Ruudk81” <Ruud.kooiman@gmail.com>
2026-06-08 17:59:21 +0200

    update

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -32,7 +32,7 @@ from config import CacheConfig
 # PIPELINE CONFIGURATION
 # =============================================================================
 
-RUN_UNTIL_STEP = 5           
+RUN_UNTIL_STEP = 6           
 FORCE_RECALCULATE_ALL = False
 VERBOSE = True
 
